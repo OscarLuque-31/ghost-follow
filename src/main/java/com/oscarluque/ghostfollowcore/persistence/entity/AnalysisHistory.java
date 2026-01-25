e2e6d5eef1dc93e461ae545a460e6b07369ae7a3
OscarLuque-31 <oscar11luquexd@gmail.com>
2026-01-25 13:32:10 +0100

    fix: error in type ID

--- src/main/java/com/oscarluque/ghostfollowcore/persistence/entity/AnalysisHistory.java
+++ src/main/java/com/oscarluque/ghostfollowcore/persistence/entity/AnalysisHistory.java
@@ -16,7 +16,7 @@ public class AnalysisHistory {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    private Integer id;
 
     @Column(name = "account_id")
     private Integer accountId;
