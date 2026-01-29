c6b01b3862d02e65c9618316c0d91a7868c31a9c
OscarLuque-31 <oscar11luquexd@gmail.com>
2026-01-29 23:33:45 +0100

    fix: error creating bean accountFollowersRepository

--- src/main/java/com/oscarluque/ghostfollowcore/persistence/repository/AccountFollowersRepository.java
+++ src/main/java/com/oscarluque/ghostfollowcore/persistence/repository/AccountFollowersRepository.java
@@ -1,13 +1,14 @@
 package com.oscarluque.ghostfollowcore.persistence.repository;
 
 import com.oscarluque.ghostfollowcore.persistence.entity.FollowerDetail;
+import com.oscarluque.ghostfollowcore.persistence.entity.FollowerId;
 import com.oscarluque.ghostfollowcore.persistence.entity.MonitoredAccount;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 import java.util.Optional;
 
-public interface AccountFollowersRepository extends JpaRepository<FollowerDetail, Integer> {
+public interface AccountFollowersRepository extends JpaRepository<FollowerDetail, FollowerId> {
 
     Optional<List<FollowerDetail>> findByAccountId(Integer accountId);
 
