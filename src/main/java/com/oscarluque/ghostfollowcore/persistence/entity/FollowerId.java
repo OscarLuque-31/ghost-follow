79cbf074177883b94374c66134916fce7a842d7f
OscarLuque-31 <oscar11luquexd@gmail.com>
2026-02-07 13:25:20 +0100

    fix: error in jpa entitys

--- src/main/java/com/oscarluque/ghostfollowcore/persistence/entity/FollowerId.java
+++ src/main/java/com/oscarluque/ghostfollowcore/persistence/entity/FollowerId.java
@@ -2,15 +2,15 @@ package com.oscarluque.ghostfollowcore.persistence.entity;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Embeddable;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
-import lombok.RequiredArgsConstructor;
 
 @Embeddable
 @Data
 @NoArgsConstructor
-@RequiredArgsConstructor
+@AllArgsConstructor
 @EqualsAndHashCode
 public class FollowerId {
 
--- src/main/java/com/oscarluque/ghostfollowcore/persistence/entity/FollowingId.java
+++ src/main/java/com/oscarluque/ghostfollowcore/persistence/entity/FollowingId.java
@@ -2,12 +2,14 @@ package com.oscarluque.ghostfollowcore.persistence.entity;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Embeddable;
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
 
 @Embeddable
 @Data
 @NoArgsConstructor
-@RequiredArgsConstructor
 @AllArgsConstructor
 @EqualsAndHashCode
 public class FollowingId {
